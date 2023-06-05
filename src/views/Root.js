14189195a449b4cb1250d74581bd9d1e38bfefcd
Kacper Gajda <lorenstenlelak@gmail.com>
2023-06-05 10:13:54 +0200

    basic folder structure

--- src/views/Root.js
+++ src/views/Root.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { GlobalStyle } from '../assets/themes/globalStyles'
 import styled, { ThemeProvider } from 'styled-components'
 import { theme } from '../assets/themes/theme'
-import Header from '../Header'
+import Header from '../components/organisms/Header'
 import LanguageSelectBar from '../components/atoms/LanguageSelectBar'
 
 const StyledWrapper = styled.div``
